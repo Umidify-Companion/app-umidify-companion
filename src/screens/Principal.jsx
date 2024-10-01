c96854364e84372980d7716a8e01f900f949d0b1
Gaspari <lcsgaspari@gmail.com>
2024-10-01 20:39:05 -0300

    making the development of all the colors and functions

--- src/screens/Principal.jsx
+++ src/screens/Principal.jsx
@@ -1,22 +1,101 @@
 import React, { useState, useRef } from 'react';
-import { View, Text,  Dimensions, ImageBackground, FlatList } from "react-native";
+import { View, Text, Dimensions, ImageBackground, FlatList } from "react-native";
 import { LinearGradient } from 'expo-linear-gradient';
 import LottieView from 'lottie-react-native';
 import { Avatar } from 'react-native-paper';
-import { Button } from 'react-native-paper';
+import { IconButton, MD3Colors } from 'react-native-paper';
 
 
 const Principal = ({ navigation }) => {
 
-	const [active, setActive] = React.useState('');
-
 	const windowWidth = Dimensions.get('window').width;
 	const windowHeight = Dimensions.get('window').height;
 
-	const data = [
-		{ id: '1', text: 'Ligue o umidificador!', subtitle: 'O ambiente está seco demais.' },
-		{ id: '2', text: 'Prepare um refresco!', subtitle: 'O ar está muito quente' }
-	];
+	const umidity = '30%'
+
+	const backgroundUmidity = () => {
+		const numericUmidity = parseInt(umidity)
+
+		switch (true) {
+			case numericUmidity >= 10 && numericUmidity <= 40:
+				return require("../assets/umidity20.json");
+			case numericUmidity >= 40 && numericUmidity <= 60:
+				return require("../assets/umidity40.json");
+			case numericUmidity >= 60 && numericUmidity <= 80:
+				return require("../assets/umidity60.json");
+			case numericUmidity >= 80 && numericUmidity <= 100:
+				return require("../assets/umidity80.json");
+			default:
+				return require("../assets/umidityDefault.json");
+		}
+	};
+
+	const iconUmidity = () => {
+		const numericUmidity = parseInt(umidity)
+
+		switch (true) {
+			case numericUmidity >= 10 && numericUmidity <= 40:
+				return require("../assets/desert.json");
+			case numericUmidity >= 40 && numericUmidity <= 60:
+				return require("../assets/umidity40logo.json");
+			case numericUmidity >= 60 && numericUmidity <= 80:
+				return require("../assets/umidity60logo.json");
+			case numericUmidity >= 80 && numericUmidity <= 100:
+				return require("../assets/umidity80logo.json");
+			default:
+				return require("../assets/umidityLoading.json");
+		}
+	}
+
+	const cardColor = () => {
+		const numericUmidity = parseInt(umidity)
+
+		switch (true) {
+			case numericUmidity >= 10 && numericUmidity <= 40:
+				return ['rgba(255, 165, 0, 1)', 'rgba(255, 255, 255, 0.1)'];
+			case numericUmidity >= 40 && numericUmidity <= 60:
+				return ['rgb(254, 222, 44)', 'rgba(255, 255, 255, 0.1)'];
+			case numericUmidity >= 60 && numericUmidity <= 80:
+				return ['rgb(153, 213, 244)', 'rgba(255, 255, 255, 0.1)'];
+			case numericUmidity >= 80 && numericUmidity <= 100:
+				return ['rgb(34, 89, 200)', 'rgba(255, 255, 255, 0.1)'];
+			default:
+				return ['rgba(0, 0, 0, 1)', 'rgba(255, 255, 255, 0.1)'];
+		}
+	}
+
+	const data = () => {
+		const numericUmidity = parseInt(umidity)
+
+		switch (true) {
+			case numericUmidity >= 10 && numericUmidity <= 40:
+				return [
+					{ id: '1', text: 'Ligue o umidificador!', subtitle: 'O ambiente está seco demais.' },
+					{ id: '2', text: 'Beba mais água!', subtitle: 'A baixa umidade pode te desidratar.' }
+				];
+			case numericUmidity >= 40 && numericUmidity <= 60:
+				return [
+					{ id: '1', text: 'Quase lá!', subtitle: 'A umidade está chegando a um bom nível.' },
+					{ id: '2', text: 'Aproveite o dia!', subtitle: 'As condições estão quase adequadas' }
+				];
+			case numericUmidity >= 60 && numericUmidity <= 80:
+				return [
+					{ id: '1', text: 'Condição perfeita!', subtitle: 'Você está com a umidade correta.' },
+					{ id: '2', text: 'Abra as janelas', subtitle: 'Evite que a umidade suba demais!' }
+				];
+			case numericUmidity >= 80 && numericUmidity <= 100:
+				return [
+					{ id: '1', text: 'Umidade muito alta', subtitle: 'O ar está bastante úmido.' },
+					{ id: '2', text: 'Deixe o ambiente ventilado', subtitle: 'Reduza a umidade do ar.' }
+				];
+			default:
+				return [
+					{ id: '1', text: 'Procurando umidade...', subtitle: 'Certifique de configurar seu sensor' }
+				];
+		}
+	};
+
+	const myData = data();
 
 	const [currentIndex, setCurrentIndex] = useState(0);
 	const flatListRef = useRef();
@@ -30,31 +109,31 @@ const Principal = ({ navigation }) => {
 	return (
 		<View className="flex-1 justify-center items-center">
 			<LottieView
-				source={require("../assets/umidity20.json")}
+				source={backgroundUmidity()}
 				style={{ width: "500%", height: "200%", position: 'absolute', zIndex: -1 }}
 				autoPlay
 				loop
 			/>
 			<View className="absolute min-h-full top-14 left-3">
 				<View className="flex flex-row items-center space-x-2">
-					<Avatar.Image size={40} source={require('../assets/fototeste.jpg')} />
-					<Text className="text-lg text-white">Bem vindo, *nome*</Text>
+					<Avatar.Image size={30} source={require('../assets/fototeste.jpg')} />
+					<Text className="text-base text-white">Bem vindo, *nome*</Text>
 				</View>
 			</View>
 			<View className="flex flex-row items-center justify-between min-w-full pl-12 pr-5">
 				<View className="flex flex-col ml-[19px]">
 					<Text className="text-3xl font-light text-white mb-3">Umidade</Text>
-					<Text className="text-7xl font-black mt-1 text-white">20%</Text>
+					<Text className="text-7xl font-black mt-1 text-white">{umidity}</Text>
 				</View>
 				<LottieView
-					source={require("../assets/desert.json")}
-					style={{ width: "70%", height: "200%" }}
+					source={iconUmidity()}
+					style={{ width: "70%", height: "150%" }}
 					autoPlay
 					loop
 				/>
 			</View>
 
-			<View className="flex flex-row w-[322px] mt-1 mb-10">
+			<View style={{ width: windowWidth * 0.8, height: 40, display: 'flex', flexDirection: 'row'}}>
 				<Text className="text-white text-lg">Temp. do ambiente: </Text>
 				<Text className="font-black text-white text-lg">20°</Text>
 			</View>
@@ -62,7 +141,7 @@ const Principal = ({ navigation }) => {
 			<View className="space-y-7">
 				{/* Dicas */}
 				<LinearGradient
-					colors={['rgba(255, 165, 0, 1)', 'rgba(255, 255, 255, 0.1)']} // Gradiente da borda com tons de laranja
+					colors={cardColor()} // Gradiente da borda com tons de laranja
 					useAngle={true}
 					angle={128.52}
 					locations={[0, 1]}
@@ -89,7 +168,7 @@ const Principal = ({ navigation }) => {
 							source={require('../assets/noise.png')}
 							resizeMode="cover"
 							style={{ flex: 1, justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}
-							imageStyle={{ borderRadius: 15, opacity: 0.6 }} // Para garantir que a imagem siga o borderRadius do card
+							imageStyle={{ borderRadius: 15, opacity: 0.8 }} // Para garantir que a imagem siga o borderRadius do card
 						>
 							<LinearGradient
 								locations={[0, 1]}
@@ -100,7 +179,7 @@ const Principal = ({ navigation }) => {
 							>
 								{/* FlatList for horizontal swipe */}
 								<FlatList
-									data={data}
+									data={myData} // Passando os dados para o FlatList
 									ref={flatListRef}
 									horizontal
 									pagingEnabled
@@ -115,9 +194,8 @@ const Principal = ({ navigation }) => {
 									)}
 								/>
 
-								{/* Pagination Dots */}
-								<View style={{ flexDirection: 'row', position: 'absolute', bottom: 10 }}>
-									{data.map((_, index) => (
+								<View style={{ flexDirection: 'row', position: 'absolute', bottom: 4 }}>
+									{myData.map((_, index) => (
 										<View
 											key={index}
 											style={{
@@ -137,7 +215,7 @@ const Principal = ({ navigation }) => {
 
 				{/* //Informações */}
 				<LinearGradient
-					colors={['rgba(255, 165, 0, 1)', 'rgba(255, 255, 255, 0.1)']} // Gradiente da borda com tons de laranja
+					colors={cardColor()} // Gradiente da borda com tons de laranja
 					useAngle={true}
 					angle={128.52}
 					locations={[0, 1]}
@@ -164,7 +242,7 @@ const Principal = ({ navigation }) => {
 							source={require('../assets/noise.png')}
 							resizeMode="cover"
 							style={{ flex: 1, justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}
-							imageStyle={{ borderRadius: 15, opacity: 0.6 }} // Para garantir que a imagem siga o borderRadius do card
+							imageStyle={{ borderRadius: 15, opacity: 0.8 }} // Para garantir que a imagem siga o borderRadius do card
 						>
 							<LinearGradient
 								locations={[0, 1]}
@@ -195,11 +273,11 @@ const Principal = ({ navigation }) => {
 			</View>
 
 			{/* Menu */}
-			<View className="bg-black w-72 h-16 absolute bottom-4 rounded-3xl flex justify-center items-center opacity-60">
-				<View className="flex flex-row space-x-3">
-				<Button icon="home" textColor='#888888' onPress={() => console.log('Pressed')}/>
-				<Button icon="cloud" textColor='#888888' onPress={() => console.log('Pressed')}/>
-				<Button icon="cog-outline" textColor='#888888' onPress={() => console.log('Pressed')}/>
+			<View className="w-72 h-16 absolute bottom-4 rounded-3xl flex justify-center items-center" style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
+				<View className="flex flex-row space-x-6">
+					<IconButton icon="home" iconColor={MD3Colors.primary100} onPress={() => console.log('Pressed')} />
+					<IconButton icon="cloud" iconColor={MD3Colors.primary100} onPress={() => console.log('Pressed')} />
+					<IconButton icon="cog-outline" iconColor={MD3Colors.primary100} onPress={() => console.log('Pressed')} />
 				</View>
 			</View>
 		</View>
