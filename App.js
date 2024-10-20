7265119f4fb2bca2df07ded614a13acd019fd7cf
Vinicius H. Sponchiado <vinicius.h.sponchiado@gmail.com>
2024-10-20 15:09:43 -0300

    refactor: restructuring navigation

--- App.js
+++ App.js
@@ -1,35 +1,6 @@
-import Login from './src/screens/Login'
-import EsqueciSenha from './src/screens/EsqueciSenha'
-import Sucesso from './src/screens/Sucesso'
-import Registro from './src/screens/Registro'
-import Principal from './src/screens/Principal'
-import PrincipalWeather from './src/screens/PrincipalWeather'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-const Stack = createStackNavigator();
-const HomeStack = createNativeStackNavigator();
-
-function PrincipalTabs() {
-  return (
-    <HomeStack.Navigator>
-      <HomeStack.Screen options={{ headerShown: false }} name="Principal" component={Principal} />
-      <HomeStack.Screen options={{ headerShown: false }} name="PrincipalWeather" component={PrincipalWeather} />
-    </HomeStack.Navigator>
-  );
-}
+import React from 'react';
+import AppNavigator from './src/navigations/AppNavigator';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen options={{ headerShown: false }} name="Login" component={Login} />
-        <Stack.Screen options={{ headerShown: false }} name="EsqueciSenha" component={EsqueciSenha} />
-        <Stack.Screen options={{ headerShown: false }} name="Sucesso" component={Sucesso} />
-        <Stack.Screen options={{ headerShown: false }} name="Registro" component={Registro} />
-        <Stack.Screen options={{ headerShown: false }} name="PrincipalTabs" component={PrincipalTabs} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <AppNavigator />;
 }
